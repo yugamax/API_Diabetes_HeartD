0806cac135c3696478b8d479d76e5638a37baade
yugamax <ayp14111@gmail.com>
2025-03-31 03:25:06 +0530

    API for diabetes and Heart disease detection

--- hd_db.py
+++ hd_db.py
@@ -63,9 +63,9 @@ async def predict_diabetes(input_data: DiabetesInput):
         input_scaled = process_diabetes(input_data)
         result = diabetes_model.predict(input_scaled)[0][0]
         if result > 0.5:
-            diag=f"You have diabetes ({str(result*100)}% confidence)"
+            diag=f"You have diabetes ({str(result*100):.2f}% confidence)"
         else:
-            diag=f"You don't have diabetes ({str(result*100)}% confidence)"
+            diag=f"You don't have diabetes ({str(result*100):.2f}% confidence)"
         return {"result": diag}
     except Exception as e:
         return {"error": str(e)}
@@ -76,9 +76,9 @@ async def predict_heart_disease(input_data: HeartDiseaseInput):
         input_scaled = process_heart_disease(input_data)
         result = heart_disease_model.predict(input_scaled)[0][0]
         if result > 0.5:
-            diag=f"You have Heart disease ({str(result*100)}% confidence)"
+            diag=f"You have Heart disease ({str(result*100):.2f}% confidence)"
         else:
-            diag=f"You don't Heart disease ({str(result*100)}% confidence)"
+            diag=f"You don't Heart disease ({str(result*100):.2f}% confidence)"
         return {"result": diag}
     except Exception as e:
         return {"error": str(e)}
